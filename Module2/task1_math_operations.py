62cff589fb35466afd9bae17196e2764c4dbf4ef
abhishek1587 <abhi_kumar1587@yahoo.co.in>
2025-11-05 16:43:52 +0530

    initial commit

---
+++ Module2/task1_math_operations.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int( input("Enter the second number:"))
+
+Addition = num1 + num2
+Substraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+print("Addition: ", Addition)
+print("Substraction: ", Substraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
